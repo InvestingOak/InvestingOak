d692a8d991c8f162eed2a83e74882201e2f3a094
stephenreynolds <git@stephenreynolds.dev>
2020-10-07 14:01:43 -0400

    Add Repository class to provide abstract methods for interacting with the database.

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,9 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using InvestingOak.Data;
-using InvestingOak.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -47,6 +47,10 @@ namespace InvestingOak
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddScoped<IRepository, Repository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
