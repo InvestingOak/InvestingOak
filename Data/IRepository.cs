0f3964a5236cbd057cc9689aa5e76736b92ad325
stephenreynolds <git@stephenreynolds.dev>
2020-10-07 14:01:43 -0400

    Add Repository class to provide abstract methods for interacting with the database.

---
+++ Data/IRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace InvestingOak.Data
+{
+    public interface IRepository
+    {
+        /// <summary>
+        /// Automatically detects changes to this context and saves to the database.
+        /// </summary>
+        /// <returns>The number of changes that were saved.</returns>
+        bool SaveAll();
+
+        /// <summary>
+        /// Automatically detects changes to this context and saves to the database.
+        /// This is the async version of SaveAll().
+        /// </summary>
+        /// <returns>The number of changes that were saved.</returns>
+        Task<bool> SaveAllAsync();
+
+        /// <summary>
+        /// Adds an entity to be tracked so that it can be saved to the database when SaveAll() is called.
+        /// </summary>
+        /// <param name="model">The entity to be tracked.</param>
+        /// <returns>The entity to be tracked.</returns>
+        EntityEntry AddEntity(object model);
+
+        /// <summary>
+        /// Adds an entity to be tracked so that it can be saved to the database when SaveAll() is called.
+        /// This is the async version of AddEntity().
+        /// </summary>
+        /// <param name="model">The entity to be tracked.</param>
+        /// <returns>The entity to be tracked.</returns>
+        Task<EntityEntry> AddEntityAsync(object model);
+
+        /// <summary>
+        /// Marks an entity as Deleted so that it may be deleted from the database when SaveAll() is called.
+        /// If the entity is already tracked then this will untrack it.
+        /// </summary>
+        /// <param name="model">The entity to delete.</param>
+        /// <returns>The entity that was deleted.</returns>
+        EntityEntry RemoveEntity(object model);
+    }
+}
---
+++ Data/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace InvestingOak.Data
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+        }
+    }
+}
