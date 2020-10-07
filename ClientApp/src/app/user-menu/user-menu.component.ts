e9182565882e4fb7b6b63290240a85f0cabef04a
stephenreynolds <git@stephenreynolds.dev>
2020-10-07 15:24:53 -0400

    Remove unneeded FontAwesome icons and document injection

--- ClientApp/src/app/user-menu/user-menu.component.ts
+++ ClientApp/src/app/user-menu/user-menu.component.ts
@@ -1,7 +1,5 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {DOCUMENT} from '@angular/common';
+import {Component, OnInit} from '@angular/core';
 import {AuthorizeService} from '../../api-authorization/authorize.service';
-import {faCaretDown, faPlus} from '@fortawesome/free-solid-svg-icons';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 
@@ -12,12 +10,10 @@ import {map} from 'rxjs/operators';
 })
 export class UserMenuComponent implements OnInit {
 
-  public caretDown = faCaretDown;
-  public plus = faPlus;
   public isAuthenticated: Observable<boolean>;
   public userName: Observable<string>;
 
-  public constructor(@Inject(DOCUMENT) public document: Document, public authorizeService: AuthorizeService) { }
+  public constructor(public authorizeService: AuthorizeService) { }
 
   public ngOnInit(): void {
     this.isAuthenticated = this.authorizeService.isAuthenticated();
