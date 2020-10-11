dc0fc408a802ef7fec3e3ae1304ac1f24db20c9b
rkenney4 <rkenney@oakland.edu>
2020-10-10 22:37:48 -0400

    Style Changes
    Changes news feed color to red

--- ClientApp/src/app/news-feed/news-feed.component.ts
+++ ClientApp/src/app/news-feed/news-feed.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-news-feed',
-  templateUrl: './news-feed.component.html'
+  templateUrl: './news-feed.component.html',
+   styleUrls: ['./news-feed.component.scss']
 })
 export class NewsFeedComponent {
 }
