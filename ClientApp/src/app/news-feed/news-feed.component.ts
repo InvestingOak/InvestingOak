26d7c556a4f8d92671fb6c3ba113311d7cdd669d
stephenreynolds <git@stephenreynolds.dev>
2020-10-13 20:19:43 -0400

    Use Finnhub to get market news

--- ClientApp/src/app/news-feed/news-feed.component.ts
+++ ClientApp/src/app/news-feed/news-feed.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FinnhubService} from '../../finnhub/finnhub.service';
+import {MarketNews} from '../../finnhub/responses';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-news-feed',
   templateUrl: './news-feed.component.html',
    styleUrls: ['./news-feed.component.scss']
 })
-export class NewsFeedComponent {
+export class NewsFeedComponent implements OnInit {
+
+  public marketNews: Observable<MarketNews[]>;
+
+  public constructor(private finnhub: FinnhubService) { }
+
+  public ngOnInit(): void {
+    this.marketNews = this.finnhub.marketNews('general');
+  }
 }
