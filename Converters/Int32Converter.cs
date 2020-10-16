4231abda8db00ac90736c3b453940e10f43fcee0
stephenreynolds <git@stephenreynolds.dev>
2020-10-15 20:18:36 -0400

    Get Company Overview and Income Statements from Alpha Vantage

---
+++ Converters/Int32Converter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace InvestingOak.Converters
+{
+    public class Int32Converter : JsonConverter<int>
+    {
+        public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+                if (int.TryParse(reader.GetString(), out int d))
+                    return d;
+
+            return default;
+        }
+
+        public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
---
+++ Models/IncomeStatementCollection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace InvestingOak.Models
+{
+    public class IncomeStatementCollection
+    {
+        public string Symbol { get; set; }
+
+        public IEnumerable<IncomeStatement> AnnualReports { get; set; }
+
+        public IEnumerable<IncomeStatement> QuarterlyReports { get; set; }
+    }
+}
