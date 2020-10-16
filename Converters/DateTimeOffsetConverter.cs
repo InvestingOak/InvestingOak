6d9e9481dd3cb179824eda4a0374df16feb58068
stephenreynolds <git@stephenreynolds.dev>
2020-10-15 20:18:36 -0400

    Get Company Overview and Income Statements from Alpha Vantage

---
+++ Converters/DateTimeOffsetConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace InvestingOak.Converters
+{
+    public class DateTimeOffsetConverter : JsonConverter<DateTimeOffset>
+    {
+        public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert,
+            JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+                if (DateTimeOffset.TryParse(reader.GetString(), out DateTimeOffset dt))
+                    return dt;
+
+            return default;
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
