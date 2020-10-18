a7b2d11445387ec498c8e2df0cde0f237ab1c1b9
stephenreynolds <git@stephenreynolds.dev>
2020-10-18 04:57:09 -0400

    Add pagination for news component

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FinnhubService} from '../../finnhub/finnhub.service';
+import {News} from '../../finnhub/responses';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  public marketNews: Observable<News[]>;
+
+  public constructor(private finnhub: FinnhubService) {
+  }
+
+  public ngOnInit(): void {
+    this.marketNews = this.finnhub.marketNews('general');
+  }
 }
--- ClientApp/src/app/news-feed/news-feed.component.ts
+++ ClientApp/src/app/news-feed/news-feed.component.ts
@@ -1,20 +1,58 @@
-import {Component, OnInit} from '@angular/core';
-import {FinnhubService} from '../../finnhub/finnhub.service';
+import {Component, Input} from '@angular/core';
 import {News} from '../../finnhub/responses';
 import {Observable} from 'rxjs';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-news-feed',
   templateUrl: './news-feed.component.html',
-   styleUrls: ['./news-feed.component.scss']
+  styleUrls: ['./news-feed.component.scss']
 })
-export class NewsFeedComponent implements OnInit {
+export class NewsFeedComponent {
 
-  public marketNews: Observable<News[]>;
+  public page = 1;
 
-  public constructor(private finnhub: FinnhubService) { }
+  @Input()
+  public news: Observable<News[]>;
 
-  public ngOnInit(): void {
-    this.marketNews = this.finnhub.marketNews('general');
+  @Input()
+  public title: string;
+
+  @Input()
+  public usePagination = true;
+
+  public pageSize = 5;
+
+  public maxSize = 10;
+
+  public filterNews(news: News[]): News[] {
+    return _.uniq(news, n => n.headline);
+  }
+
+  public getAge(timestamp: number): string {
+    const seconds = Math.floor((+new Date() - +new Date(timestamp * 1000)) / 1000);
+    if (seconds < 29) {
+      return 'Just now';
+    }
+    const intervals = {
+      'year': 31536000,
+      'month': 2592000,
+      'week': 604800,
+      'day': 86400,
+      'hour': 3600,
+      'minute': 60,
+      'second': 1
+    };
+    let counter;
+    for (const i in intervals) {
+      counter = Math.floor(seconds / intervals[i]);
+      if (counter > 0) {
+        if (counter === 1) {
+          return `${counter} ${i} ago`;
+        } else {
+          return `${counter} ${i}s ago`;
+        }
+      }
+    }
   }
 }
