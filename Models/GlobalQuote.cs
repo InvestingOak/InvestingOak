7e6847d2c91698b0c88dd70981614a43cd11da9e
stephenreynolds <git@stephenreynolds.dev>
2020-10-18 04:54:17 -0400

    Data models for AlphaVantage responses

---
+++ Models/GlobalQuote.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InvestingOak.Models
+{
+    public class GlobalQuote
+    {
+        public string Symbol { get; set; }
+
+        public decimal Open { get; set; }
+
+        public decimal High { get; set; }
+
+        public decimal Low { get; set; }
+
+        public decimal Price { get; set; }
+
+        public long Volume { get; set; }
+
+        public DateTimeOffset LatestTradingDay { get; set; }
+
+        public decimal PreviousClose { get; set; }
+
+        public decimal Change { get; set; }
+
+        public string ChangePercent { get; set; }
+    }
+}
